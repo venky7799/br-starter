e5478f000bd76bdeafe6983610dfe2b0aa0fc6f7
venky7799 <venkatesh.king50@gmail.com>
2025-04-26 00:59:42 +0530

    changes

---
+++ build/static/js/5.139a1e50.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcode=this.webpackJsonpcode||[]).push([[5],{82:function(c,e,s){"use strict";s.r(e);s(2);var n=s(0);e.default=function(){return Object(n.jsx)("div",{className:"page",children:Object(n.jsx)("div",{className:"container",children:Object(n.jsx)("div",{className:"row",children:Object(n.jsx)("h3",{children:"Under construction"})})})})}}}]);
+//# sourceMappingURL=5.139a1e50.chunk.js.map
